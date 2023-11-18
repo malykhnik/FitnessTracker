729e8a97eea4f6856cc3ef59cf2f3f7bb207c2c5
Nikita <malykhnik@mail.ru>
2023-11-18 15:58:55 +0400

    Changed functions for getting steps, heartbeat and sleepDate
    1) Information about steps for the current day to the current point in time is displayed
    2) Displays information about the last heart rate reading (available in GoogleFit)
    3) Information about the duration of sleep is displayed and information about the duration of each sleep phase is recorded in the List

--- FitnessTracker_v.4/FitnessTracker/app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ FitnessTracker_v.4/FitnessTracker/app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -7,6 +7,7 @@ import static com.google.android.gms.fitness.data.DataType.TYPE_HEART_POINTS;
 import static com.google.android.gms.fitness.data.DataType.TYPE_HEART_RATE_BPM;
 import static com.google.android.gms.fitness.data.DataType.TYPE_STEP_COUNT_DELTA;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,19 +21,26 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessActivities;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.SessionsClient;
 import com.google.android.gms.fitness.data.Bucket;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Session;
 import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.request.SessionInsertRequest;
+import com.google.android.gms.fitness.request.SessionReadRequest;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
@@ -40,21 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "FitnessTrackerLOG";
     private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
+
     private GoogleSignInAccount account;
+    private SessionsClient sessionsClient;
+
     private GoogleSignInOptionsExtension fitnessOptions;
     private DataReadRequest readRequest;
+    private SessionReadRequest sessionReadRequest;
 
     private ZonedDateTime endTime;
     private ZonedDateTime startTime;
 
+    private List<String> SLEEP_STAGE_NAMES;
+
     private List<Integer> listStepsRate;
     private List<Integer> listHeartRate;
+    private List<HashMap<String, Integer>> listStageSleep;
 
     private TextView tvStepCount;
     private TextView tvHeartCount;
     private TextView tvSleep;
     private Button refresh;
     private Button exit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +96,15 @@ public class MainActivity extends AppCompatActivity {
     private void setupVars() {
         listHeartRate = new ArrayList<>();
         listStepsRate = new ArrayList<>();
+        listStageSleep = new ArrayList<>();
+        SLEEP_STAGE_NAMES = new ArrayList<>();
+        SLEEP_STAGE_NAMES.add("Unused");
+        SLEEP_STAGE_NAMES.add("Awake (during sleep)");
+        SLEEP_STAGE_NAMES.add("Sleep");
+        SLEEP_STAGE_NAMES.add("Out-of-bed");
+        SLEEP_STAGE_NAMES.add("Light sleep");
+        SLEEP_STAGE_NAMES.add("Deep sleep");
+        SLEEP_STAGE_NAMES.add("REM sleep");
     }
 
     //ИЗ ОФ. ДОКУМЕНТАЦИИ
@@ -124,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
         //НАСТРОЙКА ВХОДА ИЗ ОФ.ДОКУМЕНТАЦИИ
         //настройка параметров фитнеса
         fitnessOptions = FitnessOptions.builder()
+                .accessSleepSessions(FitnessOptions.ACCESS_WRITE)
                 .addDataType(TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
                 .addDataType(AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
                 .addDataType(AGGREGATE_HEART_RATE_SUMMARY, FitnessOptions.ACCESS_READ)
@@ -143,15 +169,18 @@ public class MainActivity extends AppCompatActivity {
             Log.w(TAG, "accessGoogleFit");
             accessGoogleFit();
         }
+
     }
 
     private void accessGoogleFit() {
         account = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+        sessionsClient = Fitness.getSessionsClient(this, account);
     }
 
     private void getGoogleFitData(GoogleSignInAccount account) {
-        getDataSteps();
-        getDataHeartRate();
+//        getDataSteps();
+//        getDataHeartRate();
+        getDataSleep();
     }
 
     private void setSettingsForGettingSteps() {
@@ -200,8 +229,6 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         tvStepCount.setText(String.valueOf(listStepsRate.get(listStepsRate.size() - 1)));
                     }
-
-
                 })
                 .addOnFailureListener(e -> Log.e(TAG, "Failed to read steps rate data", e));
     }
@@ -259,5 +286,81 @@ public class MainActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> Log.e(TAG, "Failed to read steps rate data", e));
     }
 
+    private void setSettingsForGettingSleep() {
+        endTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
+        startTime = endTime.toLocalDate().minusDays(1).atStartOfDay(ZoneId.systemDefault());
+        Log.i(TAG, "(Sleep) Range Start: " + startTime);
+        Log.i(TAG, "(Sleep) Range End: " + endTime);
+
+        sessionReadRequest = new SessionReadRequest.Builder()
+                .readSessionsFromAllApps()
+                .includeSleepSessions()
+                .read(DataType.TYPE_SLEEP_SEGMENT)
+                .setTimeInterval(startTime.toEpochSecond(), endTime.toEpochSecond(), TimeUnit.SECONDS)
+                .build();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void getDataSleep() {
+        setSettingsForGettingSleep();
+
+        sessionsClient.readSession(sessionReadRequest)
+                .addOnSuccessListener(response -> {
+                    for (Session session : response.getSessions()) {
+                        long sessionStart = session.getStartTime(TimeUnit.MILLISECONDS);
+                        long sessionEnd = session.getEndTime(TimeUnit.MILLISECONDS);
+                        Log.i(TAG, "Sleep between " + sessionStart + " and " + sessionEnd);
+
+                        long sumSleepingTime = 0;
+                        HashMap<String, Integer> stagesAndMinutes = new HashMap<>();
+                        stagesAndMinutes.put("Unused", 0);
+                        stagesAndMinutes.put("Awake (during sleep)", 0);
+                        stagesAndMinutes.put("Sleep", 0);
+                        stagesAndMinutes.put("Out-of-bed", 0);
+                        stagesAndMinutes.put("Light sleep", 0);
+                        stagesAndMinutes.put("Deep sleep", 0);
+                        stagesAndMinutes.put("REM sleep", 0);
+
+                        // If the sleep session has finer granularity sub-components, extract them:
+                        List<DataSet> dataSets = response.getDataSet(session);
+                        for (DataSet dataSet : dataSets) {
+                            for (DataPoint point : dataSet.getDataPoints()) {
+                                int sleepStageVal = point.getValue(Field.FIELD_SLEEP_SEGMENT_TYPE).asInt();
+                                //получаем стадию сна(название)
+                                String sleepStage = SLEEP_STAGE_NAMES.get(sleepStageVal);
+                                long segmentStart = point.getStartTime(TimeUnit.MINUTES);
+                                long segmentEnd = point.getEndTime(TimeUnit.MINUTES);
+                                //считаем сколько длилась стадия сна
+                                long timeSleepingStage = segmentEnd - segmentStart;
+                                sumSleepingTime += timeSleepingStage;
+                                //увеличиваем минуты для каждой фазы сна
+                                stagesAndMinutes.put(sleepStage,
+                                        stagesAndMinutes.get(sleepStage) + (int)timeSleepingStage);
+                                Log.i(TAG, "\t* Type " + sleepStage + " between " + segmentStart + " and " + segmentEnd);
+                                Log.i(TAG, "All sleep " + timeSleepingStage);
+                            }
+                        }
+                        //добавляем сессию сна в list
+                        listStageSleep.add(stagesAndMinutes);
+                        //выводим минуты по каждой стадии на текущей сессии
+                        for (HashMap<String, Integer> hm: listStageSleep) {
+                            for (String key: hm.keySet()) {
+                                Log.i(TAG, "Stage " + key + " Minutes " + hm.get(key));
+                            }
+                        }
+                        //переводим минуты в формат (..h ..m)
+                        int hours = (int) (sumSleepingTime/60);
+                        int minutes = (int) sumSleepingTime % 60;
+                        Log.i(TAG, "Hours " + hours + " Minutes " + minutes);
+                        //Обновление данных
+                        if (hours >= 1) {
+                            tvSleep.setText(String.valueOf(hours) + "h " + String.valueOf(minutes) + "m");
+                        }else {
+                            tvSleep.setText(String.valueOf(minutes) + "m");
+                        }
+
+                    }
+                });
+    }
 
 }
